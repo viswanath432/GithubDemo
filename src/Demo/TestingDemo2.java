861e809fd65a37291b729dec0a9dde9af1e56153
user <user@DESKTOP-7PB136N>
2024-04-26 14:17:26 +0530

    Second Commit

---
+++ src/Demo/TestingDemo2.java
@@ -0,0 +1,21 @@
+package Demo;
+
+public class TestingDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String Hungry = "Yes";
+		
+		if(Hungry =="Yes")
+			
+			System.out.println("Eat Somethings");
+		
+		else {
+			System.out.println("Do work");
+		}
+		
+
+	}
+
+}
